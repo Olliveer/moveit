7476638bcc481e52f77497958a03145b64e2c6c3
olliveer <j.oliveira90@live.com>
2021-04-03 15:23:46 -0300

    delete components unauthenticated and authenticated

--- src/components/authenticated.tsx
+++
@@ -1,5 +0,0 @@
-import { GetStaticProps, InferGetServerSidePropsType } from "next";
-import React from 'react';
-import { connectToDatabase } from "../util/mongodb";
-import { Sidebar } from './Sidebar';
-
